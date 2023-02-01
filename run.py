11db97c4dd5e658fbd2626315ffcde6eb8c4d94b
NDOMINEY <natalie.r.dominey@gmail.com>
2023-02-01 23:06:08 +0000

    Add Ascii color formatting

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""imports system for ascii escape codes readability"""
+import os
+
+os.system("")
+
+
+class AnsciiFormatting:
+    """ take in name of ascii file for formatting"""
+
+    def __init__(self, text):
+        self.text = text
+
+    def color_anscii(self):
+        """ replaces color with ascii escape codes"""
+
+        font_colors = {
+            "green": "\u001b[32m",
+            "yellow": "\u001b[33;1m",
+            "blue": "\u001b[34;1m",
+            "white": "\u001b[37m",
+            "red":  "\u001b[31m",
+            "cyan": "\u001b[36m"
+        }
+
+        file = f"./assets/ascii_docs/{self.text}.txt"
+        content = open(file, "r", encoding="utf-8")
+        anscii = "".join(content.readlines())
+
+        for color in font_colors:
+            anscii = anscii.replace("[[" + color + "]]", font_colors[color])
+
+        return anscii
+
+
+title = AnsciiFormatting("title")
+
+
+print(title.color_anscii())
