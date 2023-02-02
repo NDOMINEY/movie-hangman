87f3e690919096f2791f73b33dbb9bd6c4f0e0d5
NDOMINEY <natalie.r.dominey@gmail.com>
2023-02-02 14:27:17 +0000

    Change ascii iteration to .items()

--- run.py
+++ run.py
@@ -20,14 +20,14 @@ class AnsciiFormatting:
             "white": "\u001b[37m",
             "red":  "\u001b[31m",
             "cyan": "\u001b[36m"
-        }
+            }
 
         file = f"./assets/ascii_docs/{self.text}.txt"
         content = open(file, "r", encoding="utf-8")
         anscii = "".join(content.readlines())
 
-        for color in font_colors:
-            anscii = anscii.replace("[[" + color + "]]", font_colors[color])
+        for color, code in font_colors.items():
+            anscii = anscii.replace("[[" + color + "]]", code)
 
         return anscii
 
