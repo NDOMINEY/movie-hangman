9e1c6ab4e2f9ee17557530d9e0eae75ec0915b61
NDOMINEY <natalie.r.dominey@gmail.com>
2023-02-08 17:17:11 +0000

    Add google api to run.py

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
-"""imports system for ascii escape codes readability"""
 import os
 import time
 import platform
 import random
 import string
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman_highscores')
+
+scores = SHEET.worksheet('highscore_summary')
 
 os.system("")
 
